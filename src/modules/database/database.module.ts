c1b63a014269458bfe940b6002c11b80c9c35f71
CMPNION <norkindima57@gmail.com>
2025-08-05 13:23:11 +0500

    database

---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+    imports: [SequelizeModule.forRoot({
+        logging: false,
+        dialect: "postgres",
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        dialectOptions: {
+            charset: "utf8",
+        },
+        define: {
+            charset: "utf8",
+            collate: "utf8_general_ci",
+        },
+        autoLoadModels: true,
+        synchronize: false,
+        retryAttempts: 3,
+        models: [],
+    })]
+})
+export class DatabaseModule {}
