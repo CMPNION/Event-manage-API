7526878fa4e57c61874d088e110fa717d9b45cd9
CMPNION <norkindima57@gmail.com>
2025-08-07 23:08:57 +0500

    fix

---
+++ src/modules/cache/cache.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { CacheService } from './cache.service';
+import { CacheModule as Cache } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+
+@Module({
+  imports: [
+    Cache.registerAsync({
+      isGlobal: true,
+      useFactory: () => ({
+        stores: [createKeyv(process.env.REDIS_URL)],
+      }),
+    }),
+  ],
+  providers: [CacheService],
+  exports: [CacheService],
+})
+export class CacheModule {}
